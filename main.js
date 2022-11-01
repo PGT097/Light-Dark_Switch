dca4a037537a54d2d1490b415fabd90c2b1f9d84
George <gheorghepopovici007@gmail.com>
2022-11-01 16:03:26 +0200

    upd

--- main.js
+++ main.js
@@ -1,35 +1,35 @@
 "use strict";
 const body = document.querySelector("body");
 const h2 = document.createElement("h2");
+const button = document.createElement("button");
 const paragraph = document.createElement("p");
-const img = document.createElement("img");
 
 body.append(h2);
-body.append(img);
+body.append(button);
 body.append(paragraph);
-
 body.style.backgroundColor = "black";
 
 h2.textContent = "Dark/Light Mode Switcher";
 h2.style.color = "white";
-img.src = "https://cdn-icons-png.flaticon.com/512/180/180700.png";
-img.style.filter = "invert(1)";
-img.style.width = "50px";
-img.style.height = "50px";
+
+button.style.background = "black";
+button.style.width = "50px";
+button.style.height = "50px";
+button.textContent = "🌛";
 
 paragraph.textContent = "Just press the button above to toggle!";
 paragraph.style.color = "White";
 
-img.addEventListener("click", () => {
+button.addEventListener("click", () => {
   if (body.style.backgroundColor === "black") {
     body.style.backgroundColor = "white";
     h2.style.color = "black";
     paragraph.style.color = "black";
-    img.style.filter = "invert(0)";
+    button.style.backgroundColor = "black";
   } else if (body.style.backgroundColor === "white") {
     body.style.backgroundColor = "black";
     h2.style.color = "white";
     paragraph.style.color = "white";
-    img.style.filter = "invert(1)";
+    button.style.backgroundColor = "white";
   }
 });
